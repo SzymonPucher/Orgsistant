81bf3e8bc815aad968849df4853e9113bfd042ab
SzymonPucher <spucher@inteira.eu>
2018-10-11 13:53:49 +0200

    Nice basic table

--- organizer/views.py
+++ organizer/views.py
@@ -3,6 +3,6 @@ from .models import Category, PomodoroSession, ToDoItem
 
 # Create your views here.
 def index(request):
-    todos = ToDoItem.objects.all()
+    todos = ToDoItem.objects.order_by('category')
     context = {'todo': todos}
     return render(request, 'organizer/index.html', context)
